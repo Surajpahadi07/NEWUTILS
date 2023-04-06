5845ff5a8c39439dfb796e0cd9cb5fa8971e44f5
rahul <rahulchauhah50@gmail.com>
2023-04-06 09:59:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Textform from './Components/Textform';
+
+function App() {
+  return (
+    <>
+    <Navbar title = "Blog" name="rahul" com = "About"/>
+    <Textform />
+    </>
+  );
+}
+export default App;
---
+++ src/Components/Textform.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function Textform() {
+    const[text,setText] = useState("");
+
+    const handleOnChange = (event) =>{
+        setText(event.target.value)
+    }
+    
+    const handleOnUpper = (event)=>{
+       setText(text.toUpperCase());
+    
+    }
+    const handleOnLower = (event)=>{
+       setText(text.toLocaleLowerCase());
+    }
+
+    const handleProper = (event)=>{
+      let tempArr = text.split(" ");
+      let temp = "";
+      tempArr.forEach(element => {
+        element = element.charAt(0).toUpperCase()+element.substr(1,element.length)
+        temp = temp+" "+element;
+        temp = temp.trim();
+        setText(temp)
+        
+       });
+    }
+
+  return (
+    <div className="mb-3" id="rahul">
+    <label className="form-label">Enter your text</label>
+    <textarea 
+    className="form-control p-3 mr-2 ml-2" 
+    id="exampleFormControlTextarea1" 
+    rows="3"
+    value={text}
+    placeholder="Enter Text Here"
+    onChange={handleOnChange}>
+
+    </textarea>
+    <button className='btn btn-primary m-2' onClick={handleOnUpper}>Upper</button>
+    <button className='btn btn-primary m-2' onClick={handleOnLower}>Lower</button>
+    <button className='btn btn-primary m-2' onClick={handleProper}>Proper</button>
+  </div>
+  )
+}
