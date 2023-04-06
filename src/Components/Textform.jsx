f4ef004bc6d5af52685b0abc1132a462240aaf96
rahul <rahulchauhah50@gmail.com>
2023-04-06 10:25:06 +0530

    first commit

--- src/Components/Textform.jsx
+++ src/Components/Textform.jsx
@@ -14,12 +14,15 @@ export default function Textform() {
     const handleOnLower = (event)=>{
        setText(text.toLocaleLowerCase());
     }
+    const clear = (event)=>{
+       setText("");
+    }
 
     const handleProper = (event)=>{
       let tempArr = text.split(" ");
       let temp = "";
       tempArr.forEach(element => {
-        element = element.charAt(0).toUpperCase()+element.substr(1,element.length)
+        element = element.charAt(0).toUpperCase()+element.substr(1,element.length).toLowerCase();
         temp = temp+" "+element;
         temp = temp.trim();
         setText(temp)
@@ -31,7 +34,7 @@ export default function Textform() {
     <div className="mb-3" id="rahul">
     <label className="form-label">Enter your text</label>
     <textarea 
-    className="form-control p-3 mr-2 ml-2" 
+    className="form-control p-3 container" 
     id="exampleFormControlTextarea1" 
     rows="3"
     value={text}
@@ -39,9 +42,18 @@ export default function Textform() {
     onChange={handleOnChange}>
 
     </textarea>
-    <button className='btn btn-primary m-2' onClick={handleOnUpper}>Upper</button>
-    <button className='btn btn-primary m-2' onClick={handleOnLower}>Lower</button>
-    <button className='btn btn-primary m-2' onClick={handleProper}>Proper</button>
+    <div className="container my-3">
+      <button className='btn btn-primary m-2' onClick={handleOnUpper}>Upper</button>
+      <button className='btn btn-primary m-2' onClick={handleOnLower}>Lower</button>
+      <button className='btn btn-primary m-2' onClick={handleProper}>Proper</button>
+      <button className='btn btn-primary m-2' onClick={clear}>Clear</button>
+      <div>
+        <h2>Your Text Summary</h2>
+        <p>Your text Contains {text.split(" ").length-1} words and {text.length}</p>
+        <h2>Preview</h2>
+        <p className="b-2">{text}</p>
+      </div>
+    </div>
   </div>
   )
 }
